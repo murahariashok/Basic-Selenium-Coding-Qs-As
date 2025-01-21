d13268c9fa2c3b4a041dfbb45d44a589841f592a
ASHOK <murahariashok16@gmail.com>
2025-01-21 14:48:48 +0530

    add the theory of sselenium

---
+++ 1.SeleniumTheory.py
@@ -0,0 +1,29 @@
+'''
+
+1.what is selenium
+
+selenium is free or open source tool used for automating web based applications.
+It supports multiple languages like java,python ,R,c# and also supports multiple browsers
+
+2.Types of selenium Components
+there are multiple types of components like
+a. Selenium WebDriver
+:most popular commonly used tool. it is a browser automation framework that allow you to
+execute your test scripts directly on web browser
+Direct control the web browser to perform actions like click , type, navigation
+
+b. Selenium RC(remote control)
+:older version of selenium webdriver not widely used anymore , it used to work on
+javascript to automate the web application
+
+c. Selenium IDE(integrated Development Environment)
+:browser plugin(firefox) for quick and simple testing
+allow to record and reply browsers action without coding
+
+d. Selenium Grid
+:used to run the tests on multiple machines parallely
+used in different browsers and environments
+
+
+
+'''
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Basic-Selenium-Coding-Qs-As
+
